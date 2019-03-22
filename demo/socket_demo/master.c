c141d1c514491dbc262786c961d471aeade2aaf2
XXXXzzzz000 <119524428@qq.com>
2019-03-22 11:57:35 +0800

    添加注释

--- demo/socket_demo/master.c
+++ demo/socket_demo/master.c
@@ -2,16 +2,17 @@
 // Created by MightyPork on 2017/10/15.
 //
 
-#include <stdio.h>
 #include "../demo.h"
+#include <stdio.h>
 
+/* 测试用id监听器 */
 TF_Result testIdListener(TinyFrame *tf, TF_Msg *msg)
 {
     printf("testIdListener()\n");
     dumpFrameInfo(msg);
     return TF_CLOSE;
 }
-
+/* 测试用全局监听器 */
 TF_Result testGenericListener(TinyFrame *tf, TF_Msg *msg)
 {
     printf("testGenericListener()\n");
@@ -22,13 +23,15 @@ TF_Result testGenericListener(TinyFrame *tf, TF_Msg *msg)
 int main(void)
 {
     demo_tf = TF_Init(TF_MASTER);
+    /* 添加全局监听器 */
     TF_AddGenericListener(demo_tf, testGenericListener);
-
+    /* demo 初始化 */
     demo_init(TF_MASTER);
-
+    /* 发送 */
     TF_SendSimple(demo_tf, 1, (pu8) "Ahoj", 5);
+    
     TF_SendSimple(demo_tf, 1, (pu8) "Hello", 6);
-
+    /* 查询 */
     TF_QuerySimple(demo_tf, 2, (pu8) "Query!", 6, testIdListener, 0);
 
     demo_sleep();
